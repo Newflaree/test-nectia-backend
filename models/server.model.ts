f30702ea1ebafe94fdcdf252aaa1ed46d5bf49b2
Camilo López <camiloj.lopez79@gmail.com>
2022-05-29 13:47:00 -0400

    Se configuran los middlewares y los path de la api

--- models/server.model.ts
+++ models/server.model.ts
@@ -1,12 +1,27 @@
 import express, { Application } from 'express';
+import cors from 'cors';
+// Interfaces
+import { ApiPaths } from '../interfaces/api-interfaces';
 
 class Server {
   private app: Application;
   private port: string;
+  private apiPaths: ApiPaths;
 
   constructor() {
     this.app = express();
-    this.port = process.env.PORT || '3001';
+    this.port = process.env.PORT || '3002';
+    this.apiPaths = {
+      auth: '/api/auth'
+    };
+
+    // Init Methods
+    this.middlewares();
+  }
+
+  middlewares() {
+    this.app.use( cors() );
+    this.app.use( express.json() );
   }
 
   listen() {
