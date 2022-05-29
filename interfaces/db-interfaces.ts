8cf0cf00a232d0b36b0f7177ece381e7294073c1
Camilo López <camiloj.lopez79@gmail.com>
2022-05-29 19:21:14 -0400

    Se crea el modelo de laptop

--- interfaces/db-interfaces.ts
+++ interfaces/db-interfaces.ts
@@ -1,7 +1,20 @@
+import { ObjectId } from "mongoose";
+
 export interface UserProps {
   name: string;
   email: string;
   password: string;
   role: string;
+  status: boolean;
+}
+
+export interface LaptopProps {
+  name: string;
+  brand: string;
+  proce: string;
+  ram: string;
+  storage: string;
+  screen: string;
+  user: ObjectId;
   status: boolean;
 }
---
+++ models/laptop.model.ts
@@ -0,0 +1,49 @@
+import { Schema, model } from 'mongoose';
+// Interfaces
+import { LaptopProps } from '../interfaces/db-interfaces';
+
+const LaptopSchema = new Schema<LaptopProps>({
+  name: {
+    type: String,
+    required: [ true, 'Laptop name is required' ],
+    unique: true
+  },
+  brand: {
+    type: String,
+    required: [ true, 'Laptop brand is required' ]
+  },
+  proce: {
+    type: String,
+    required: [ true, 'Laptop processor is required' ]
+  },
+  ram: {
+    type: String,
+    required: [ true, 'Laptop ram is required' ]
+  },
+  storage: {
+    type: String,
+    required: [ true, 'Laptop storage is required' ]
+  },
+  screen: {
+    type: String,
+    required: [ true, 'Laptop brand is required' ],
+    
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  status: {
+    type: Boolean,
+    default: true
+  }
+});
+
+LaptopSchema.methods.toJSON = function() {
+  const { __v, _id, ...laptop } = this.toObject();
+  laptop.id = _id;
+  return laptop;
+}
+
+export default model<LaptopProps>( 'Laptop', LaptopSchema );
